ece060c452686dbbea189ddfa1ec721a147d4811
Anbil97 <126085127+Anbil97@users.noreply.github.com>
2023-11-19 19:29:29 +0530

    Add files via upload

---
+++ Document.js
@@ -0,0 +1,8 @@
+const header = document.getElementById('header');
+const paragraphs = document.getElementsByClassName('paragraph');
+
+//const title = document.getElementById('title');
+//title.textContent = 'New Title';
+
+//const parentElement = document.getElementById('parent');
+//const firstChild = parentElement.firstChild;
---
+++ windows.js
@@ -0,0 +1,11 @@
+const globalVar = 'I am global!';
+console.log(window.globalVar); // Outputs: 'I am global!'
+
+
+//setTimeout(() => {
+  //  console.log('Delayed message');
+//}, 1000);
+
+//alert('Hello, World!');
+//console.log('Logging a message');
+
